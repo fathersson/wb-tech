f8b5476ea5d0f9492cec52b87b1696e11b2432c7
Zarabelm <domogarovwin@gmail.com>
2025-11-30 22:54:01 +0300

    1.9 done

--- level1/1.7.go
+++ level1/1.7.go
@@ -1,41 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 // Mutex защищает map от одновременной записи из нескольких горутин.
 // WaitGroup используется, чтобы дождаться завершения всех горутин.
 // и чтобы программа не завершилась, пока они не закончат свою работу
 // суть работы программу - конкурентно с помощью двух горутин
 // записать в цикле данные в мапу и завершить программу
-func main() {
-	m := make(map[int]int, 100)
+// func main() {
+// 	m := make(map[int]int, 100)
 
-	var mu sync.Mutex
+// 	var mu sync.Mutex
 
-	var wg sync.WaitGroup
-	wg.Add(2)
+// 	var wg sync.WaitGroup
+// 	wg.Add(2)
 
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 100; i++ {
-			mu.Lock()
-			m[i] = 1
-			mu.Unlock()
-		}
-	}()
+// 	go func() {
+// 		defer wg.Done()
+// 		for i := 0; i < 100; i++ {
+// 			mu.Lock()
+// 			m[i] = 1
+// 			mu.Unlock()
+// 		}
+// 	}()
 
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 100; i++ {
-			mu.Lock()
-			m[i] = 2
-			mu.Unlock()
-		}
-	}()
+// 	go func() {
+// 		defer wg.Done()
+// 		for i := 0; i < 100; i++ {
+// 			mu.Lock()
+// 			m[i] = 2
+// 			mu.Unlock()
+// 		}
+// 	}()
 
-	wg.Wait()
-	fmt.Println(m)
-}
+// 	wg.Wait()
+// 	fmt.Println(m)
+// }
