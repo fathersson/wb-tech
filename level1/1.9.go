686bead13c524f333531b61eedcfc6968c2e456c
Zarabelm <domogarovwin@gmail.com>
2025-12-01 13:43:35 +0300

    done 1.10

--- level1/1.9.go
+++ level1/1.9.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 // Создаем два канала, слайс исходных чисел и WaitGroup,
 // чтобы дождаться завершения всех горутин конвейера
 //
@@ -18,41 +13,41 @@ import (
 //
 // Цикл range по каналу будет продолжать блокироваться и читать значения
 // из канала до тех пор, пока канал не будет закрыт
-func main() {
-	ch1 := make(chan int, 5)
-	ch2 := make(chan int, 5)
+// func main() {
+// 	ch1 := make(chan int, 5)
+// 	ch2 := make(chan int, 5)
 
-	var wg sync.WaitGroup
+// 	var wg sync.WaitGroup
 
-	// x := []int{1,2,3,4,5}
-	x := make([]int, 0, 5)
-	for i := 0; i < cap(x); i++ {
-		x = append(x, i+1)
-	}
+// 	// x := []int{1,2,3,4,5}
+// 	x := make([]int, 0, 5)
+// 	for i := 0; i < cap(x); i++ {
+// 		x = append(x, i+1)
+// 	}
 
-	wg.Add(3)
-	go func() {
-		defer wg.Done()
-		for i := 0; i < len(x); i++ {
-			ch1 <- x[i]
-		}
-		close(ch1)
-	}()
+// 	wg.Add(3)
+// 	go func() {
+// 		defer wg.Done()
+// 		for i := 0; i < len(x); i++ {
+// 			ch1 <- x[i]
+// 		}
+// 		close(ch1)
+// 	}()
 
-	go func() {
-		defer wg.Done()
-		for v := range ch1 {
-			ch2 <- v * 2
-		}
-		close(ch2)
-	}()
+// 	go func() {
+// 		defer wg.Done()
+// 		for v := range ch1 {
+// 			ch2 <- v * 2
+// 		}
+// 		close(ch2)
+// 	}()
 
-	go func() {
-		defer wg.Done()
-		for v := range ch2 {
-			fmt.Printf("%d ", v)
-		}
-	}()
+// 	go func() {
+// 		defer wg.Done()
+// 		for v := range ch2 {
+// 			fmt.Printf("%d ", v)
+// 		}
+// 	}()
 
-	wg.Wait()
-}
+// 	wg.Wait()
+// }
